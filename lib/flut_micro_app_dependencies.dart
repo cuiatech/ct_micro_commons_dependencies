5959ca76daf7a3baeea314e500101d8863efe7a2
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-26 02:13:32 -0300

    first commit

---
+++ lib/flut_micro_app_dependencies.dart
@@ -0,0 +1,5 @@
+library flut_micro_app_dependencies;
+
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:url_strategy/url_strategy.dart';
