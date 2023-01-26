a72840243bf2e3296e852dcb732cf10af9c6071b
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-26 15:22:38 -0300

    add shared preferences

--- lib/flut_micro_commons_dependencies.dart
+++ lib/flut_micro_commons_dependencies.dart
@@ -3,3 +3,5 @@ library flut_micro_commons_dependencies;
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:flutter_mobx/flutter_mobx.dart';
 export 'package:url_strategy/url_strategy.dart';
+export 'package:dio/dio.dart';
+export 'package:shared_preferences/shared_preferences.dart';
