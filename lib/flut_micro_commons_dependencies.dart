d1d55e537c133d90c2104a9f9f1d72c9efcc4440
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-27 17:52:04 -0300

    update

--- lib/flut_micro_commons_dependencies.dart
+++ lib/flut_micro_commons_dependencies.dart
@@ -5,3 +5,4 @@ export 'package:flutter_mobx/flutter_mobx.dart';
 export 'package:url_strategy/url_strategy.dart';
 export 'package:dio/dio.dart';
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:google_fonts/google_fonts.dart';
