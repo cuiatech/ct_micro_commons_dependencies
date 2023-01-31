6fdcf66a5f01d6f945f422a5edacde8c9d0bbf9d
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-31 00:07:46 -0300

    i18n

--- lib/flut_micro_commons_dependencies.dart
+++ lib/flut_micro_commons_dependencies.dart
@@ -1,5 +1,6 @@
 library flut_micro_commons_dependencies;
 
+export 'package:localization/localization.dart';
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:flutter_mobx/flutter_mobx.dart';
 export 'package:url_strategy/url_strategy.dart';
